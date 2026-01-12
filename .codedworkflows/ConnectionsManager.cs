0470d6c35e73792cc1b5509e3b1ba5e280539012
Steven (D2D Automation) <steven@daytodayautomation.com>
2026-01-12 19:19:43 +1100

    First commit

---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace UiBank
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
